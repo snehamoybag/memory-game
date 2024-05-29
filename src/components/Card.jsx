1161c4b52184ae578e9ecd16f154dfbafb2cab49
snehamoybag <snehamoybag2@gmail.com>
2024-05-29 19:44:46 +0530

    feat(card): add a loading text when card images are loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import "../styles/card.css";
 
 const Card = ({ name, id }) => {
+  const [isPlaceholderActive, setIsPlaceholderActive] = useState(true);
+
+  const handleImgLoad = () => setIsPlaceholderActive(false);
+
   const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   const imgAlt = `Photo of ${name}, a Pokemon`;
 
   return (
     <button type="button" className="card">
-      <img src={imgUrl} alt={imgAlt} className="card__img" />
-      <h2 className="card__name">{name}</h2>
+      {isPlaceholderActive && (
+        <p className="card__placeholder">
+          Image is loading, please wait a sec..
+        </p>
+      )}
+
+      <img
+        src={imgUrl}
+        alt={imgAlt}
+        className="card__img"
+        onLoad={handleImgLoad}
+      />
+      <h2 className="card__name">{isPlaceholderActive ? "Loading.." : name}</h2>
     </button>
   );
 };
